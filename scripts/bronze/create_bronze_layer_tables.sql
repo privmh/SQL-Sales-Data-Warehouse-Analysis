437eab54e581727d19160b09640e01d2fe4ad3b1
Mohamed Kamal <156097050+analystmk@users.noreply.github.com>
2025-06-10 00:17:46 +0300

    Update create_bronze_layer_tables.sql

--- scripts/bronze/create_bronze_layer_tables.sql
+++ scripts/bronze/create_bronze_layer_tables.sql
@@ -18,78 +18,82 @@
 --------------------------------------------------------------------------------
 */
 
+-- Drop and Create bronze.crm_cust_info
 IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info
+    DROP TABLE bronze.crm_cust_info;
 GO
-CREATE TABLE bronze.crm_cust_info(
-	cst_id				INT,
-	cst_key				NVARCHAR(50),
-	cst_firstname		NVARCHAR(50),
-	cst_lastname		NVARCHAR(50),
-	cst_marital_status	NVARCHAR(50),
-	cst_gndr			NVARCHAR(50),
-	cst_create_date		DATE
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
 GO
 
+-- Drop and Create bronze.crm_prd_info
 IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
-	DROP TABLE bronze.crm_prd_info
+    DROP TABLE bronze.crm_prd_info;
 GO
-CREATE TABLE bronze.crm_prd_info(
-	prd_id			INT,
-	prd_key			NVARCHAR(50),
-	prd_nm			NVARCHAR(50),
-	prd_cost		INT,
-	prd_line		NVARCHAR(50),
-	prd_start_dt	DATETIME,
-	prd_end_dt		DATETIME
-
+CREATE TABLE bronze.crm_prd_info (
+    prd_id         INT,
+    prd_key        NVARCHAR(50),
+    prd_nm         NVARCHAR(50),
+    prd_cost       INT,
+    prd_line       NVARCHAR(50),
+    prd_start_dt   DATETIME,
+    prd_end_dt     DATETIME
 );
 GO
 
+-- Drop and Create bronze.crm_sales_details
 IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details
+    DROP TABLE bronze.crm_sales_details;
 GO
-CREATE TABLE bronze.crm_sales_details(
-	sls_ord_num		NVARCHAR(50),
-	sls_prd_key		NVARCHAR(50),
-	sls_cust_id		INT,
-	sls_order_dt	INT,
-	sls_ship_dt		INT,
-	sls_due_dt		INT,
-	sls_sales		INT,
-	sls_quantity	INT,
-	sls_price		INT
-
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num    NVARCHAR(50),
+    sls_prd_key    NVARCHAR(50),
+    sls_cust_id    INT,
+    sls_order_dt   INT,
+    sls_ship_dt    INT,
+    sls_due_dt     INT,
+    sls_sales      INT,
+    sls_quantity   INT,
+    sls_price      INT
 );
 GO
 
+-- Drop and Create bronze.erp_cust_az12
 IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
-	DROP TABLE bronze.erp_cust_az12
+    DROP TABLE bronze.erp_cust_az12;
 GO
-CREATE TABLE bronze.erp_cust_az12(
-	cid		NVARCHAR(50),
-	bdate	DATE,
-	gen		NVARCHAR(50)
+CREATE TABLE bronze.erp_cust_az12 (
+    cid     NVARCHAR(50),
+    bdate   DATE,
+    gen     NVARCHAR(50)
 );
 GO
 
+-- Drop and Create bronze.erp_loc_101
 IF OBJECT_ID('bronze.erp_loc_101','U') IS NOT NULL
-	DROP TABLE bronze.erp_loc_101
+    DROP TABLE bronze.erp_loc_101;
 GO
-CREATE TABLE bronze.erp_loc_101(
-	cid		NVARCHAR(50),
-	cntry	NVARCHAR(50)
+CREATE TABLE bronze.erp_loc_101 (
+    cid     NVARCHAR(50),
+    cntry   NVARCHAR(50)
 );
 GO
 
+-- Drop and Create bronze.erp_px_cat_g1v2
 IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_g1v2
+    DROP TABLE bronze.erp_px_cat_g1v2;
 GO
-CREATE TABLE bronze.erp_px_cat_g1v2(
-	id			NVARCHAR(50),
-	cat			NVARCHAR(50),
-	subcat		NVARCHAR(50),
-	maintenance NVARCHAR(50)
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
 );
 GO
