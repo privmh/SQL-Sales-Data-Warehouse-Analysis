f3e5ec939ca5748a21e6961656d6a0b1d5258cd6
Mohamed Kamal <156097050+analystmk@users.noreply.github.com>
2025-06-13 14:50:00 +0300

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,50 +1,66 @@
-/*
-===============================================================================
-DDL Script: Validate Gold Layer Views
-===============================================================================
-Script Purpose:
-    This script performs validation checks on views in the Gold layer of the 
-    data warehouse. The Gold layer contains the final dimension and fact tables 
-    structured in a Star Schema for analytical use.
-
-    These checks help ensure data integrity by identifying:
-        - Inconsistencies in gender data between source systems
-        - Duplicate keys in dimension tables
-        - Orphaned records in the fact table (missing dimension references)
-
-Usage:
-    - Run this script after creating or updating Gold layer views to verify 
-      data quality before using them in analytics or reporting.
-===============================================================================
-*/
--- ====================================================================
--- Checking 'gold.dim_customers'
--- ====================================================================
-SELECT DISTINCT 
-	ci.cst_gndr AS gender,    
-	ca.gen		AS gender 
-FROM	silver.crm_cust_info ci
+IF OBJECT_ID('gold.dim_customers','V') IS NOT NULL
+	DROP VIEW gold.dim_customers;
+GO
+CREATE VIEW gold.dim_customers AS 
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY	ci.cst_id) AS customer_key, -- Surrogate key
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	ci.cst_marital_status AS marital_status,
+	CASE 
+		WHEN ci.cst_gndr <> 'n/a' THEN  ci.cst_gndr -- CRM is the primary source for gender
+		ELSE COALESCE(ca.gen,'n/a')					-- Fallback to ERP data
+	END AS gender,
+	ci.cst_create_date AS create_date,
+	ca.bdate AS birth_date,
+	la.cntry AS country
+FROM		silver.crm_cust_info ci
 LEFT JOIN	silver.erp_cust_az12 ca
-    ON ci.cst_key = ca.cid;
-
-SELECT		customer_key , COUNT(*) 
-FROM		gold.dim_customers
-GROUP BY	customer_key
-HAVING		COUNT(*)>1;
--- ====================================================================
--- Checking 'gold.product_key'
--- ====================================================================
-SELECT product_key,count(*)  
-FROM gold.dim_products
-GROUP BY product_key
-HAVING count(*)>1;
--- ====================================================================
--- Checking 'gold.fact_sales'
--- ====================================================================
-SELECT *
-FROM gold.fact_sales s 
-LEFT JOIN gold.dim_customers c
-	ON s.customer_key = c.customer_key
-LEFT JOIN gold.dim_products  p
-	ON s.product_key = p.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL;
+ON			ci.cst_key = ca.cid
+LEFT JOIN	silver.erp_loc_a101 la
+ON			ci.cst_key = la.cid ;
+------------------------------------------------------------------------------
+IF OBJECT_ID('gold.dim_products','V') IS NOT NULL
+	DROP VIEW gold.dim_products;
+GO
+CREATE VIEW gold.dim_products AS 
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt , pn.prd_key) AS product_key, -- Surrogate key
+	pn.prd_id AS product_id, 
+	pn.cat_id AS category_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date,
+	pc.cat AS category,
+	pc.subcat AS sub_category,
+	pc.maintenance
+FROM silver.crm_prd_info pn
+INNER JOIN silver.erp_px_cat_g1v2 pc
+	ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+GO
+---------------------------------------------------------------------
+IF OBJECT_ID('gold.fact_sales','V') IS NOT NULL
+	DROP VIEW gold.fact_sales;
+GO
+CREATE VIEW gold.fact_sales AS
+SELECT
+	sd.sls_ord_num,
+	p.product_key,
+	c.customer_key,
+	sd.sls_order_dt,
+	sd.sls_ship_dt,
+	sd.sls_due_dt,
+	sd.sls_sales,
+	sd.sls_quantity,
+	sd.sls_price
+FROM silver.crm_sales_details sd
+INNER JOIN gold.dim_customers c
+	ON sd.sls_cust_id = c.customer_id
+INNER JOIN gold.dim_products p
+	ON sd.sls_prd_key = p.product_number;
+GO
